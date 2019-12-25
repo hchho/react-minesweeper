0aa94c1881b673c7f774ab0428385e21c7ede142
Henry Ho <henrychho89@gmail.com>
2019-12-25 14:32:54 -0800

    Fixed delay after game error

--- src/timer/Timer.component.js
+++ src/timer/Timer.component.js
@@ -4,20 +4,22 @@ import './Timer.scss'
 
 const SECOND_IN_MS = 1000 
 
+const DELAY_IN_MS = 10
+
 const Timer = ({ endGame, gameStatus, timeLimit }) => {
   const [count, setCount] = useState(0)
 
   setTimeout(() => { 
     if (isGameActive(gameStatus) && !isGamePaused(gameStatus)) {
-      setCount(count + SECOND_IN_MS) 
+      setCount(count + DELAY_IN_MS) 
     }
-  }, SECOND_IN_MS)
+  }, DELAY_IN_MS)
 
   if (count === timeLimit) endGame()
   
   return (
     <div className='timer--counter'>
-      {count / SECOND_IN_MS}
+      {(count / SECOND_IN_MS).toFixed(2)}
     </div>
   )
 }
