0cebfdd859a08322794351c4b2ea0c80a47363a5
Henry Ho <henrychho89@gmail.com>
2019-12-26 19:02:30 -0800

    Moved keys to .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import app from 'firebase/app'
 
 const config = {
-  apiKey: "AIzaSyBpCbu5WsEORk15RrbCEZBqA_K_8W_N7rQ",
-  authDomain: "react-minesweeper-841a3.firebaseapp.com",
-  databaseURL: "https://react-minesweeper-841a3.firebaseio.com",
-  projectId: "react-minesweeper-841a3",
-  storageBucket: "react-minesweeper-841a3.appspot.com",
-  messagingSenderId: "629672203063",
-  appId: "1:629672203063:web:2ce61acceb9def662a00b2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 export const Firebase = app.initializeApp(config)
