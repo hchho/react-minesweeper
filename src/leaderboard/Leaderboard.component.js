5bdce0ce79b5644cb1393439fabea72ebe718990
Henry Ho <henrychho89@gmail.com>
2019-12-27 15:46:45 -0800

    Resolved modal issue

--- src/leaderboard/Leaderboard.component.js
+++ src/leaderboard/Leaderboard.component.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import "./Leaderboard.scss"
 
 export const Leaderboard = () => (
   <div className="leaderboard__container">
     <h3>Leaderboard</h3>
+    <p>asdf</p>
+    <p>asdf</p>
+    <p>asdf</p>
+    <p>asdf</p>
+    <p>asdf</p>
+    <p>asdf</p>
+    <p>asdf</p>
+    <p>asdf</p>
   </div>
 )
--- src/modal/Modal.component.js
+++ src/modal/Modal.component.js
@@ -1,6 +1,19 @@
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
 const modalRoot = document.getElementById("modal-root");
 
-export const Modal = ({ children }) =>
-  ReactDOM.createPortal(children, modalRoot);
+export const Modal = ({ children }) => {
+  useEffect(() => {
+    modalRoot.setAttribute("z-index", "1");
+    modalRoot.setAttribute("width", "100%");
+    modalRoot.setAttribute("height", "100%");
+    return () => {
+      modalRoot.setAttribute("z-index", "-1");
+      modalRoot.setAttribute("width", "");
+      modalRoot.setAttribute("height", "");
+    };
+  });
+
+  return ReactDOM.createPortal(children, modalRoot);
+};
