4ac2eb713c4da0403efb554307a312b1be813165
Henry Ho <henrychho89@gmail.com>
2020-01-02 15:33:11 -0800

    Display template for leaderboard

--- src/leaderboard/Leaderboard.component.js
+++ src/leaderboard/Leaderboard.component.js
@@ -1,24 +1,35 @@
-import React, { useEffect } from 'react'
-import "./Leaderboard.scss"
+import React, { useEffect, useState } from "react";
+import "./Leaderboard.scss";
 
 export const Leaderboard = ({ firebase }) => {
+  const [isFetching, setFetching] = useState(true);
+  const [leaderboard, setLeaderboard] = useState([]);
   useEffect(() => {
-    firebase.getAllScores().orderByChild("timeInMs").limitToFirst(10).once('value').then(snapshot => {
-      snapshot.forEach(data => {
-        console.log(data.val())
-      })
-    })
-  }, [firebase])
-  return (
-  <div className="leaderboard__container">
-    <h3>Leaderboard</h3>
-    <p>asdf</p>
-    <p>asdf</p>
-    <p>asdf</p>
-    <p>asdf</p>
-    <p>asdf</p>
-    <p>asdf</p>
-    <p>asdf</p>
-    <p>asdf</p>
-  </div> 
-)}
+    firebase
+      .getAllScores()
+      .orderByChild("timeInMs")
+      .limitToFirst(10)
+      .once("value")
+      .then(snapshot => {
+        snapshot.forEach(data => {
+          setLeaderboard(prevState => [...prevState, data.val()]);
+        });
+        setFetching(false)
+      });
+  }, [firebase]);
+
+  return isFetching ? (
+    "Loading..."
+  ) : (
+    <div className="leaderboard__container">
+      <h3>Leaderboard</h3>
+      <ol>
+        {leaderboard.map(data => (
+          <li key={data.timeInMs}>
+            {data.user}: {data.timeInMs}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
